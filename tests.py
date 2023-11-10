3d726bd148ab271c39b3c4a48b301c1d4332c977
Anastasiya Nikitina <anastasia.nikitina@aliexpress.ru>
2023-11-10 15:26:25 +0300

    added fourth test test_get_book_genre_check_comedy

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import pytest
 
 class TestBooksCollector:
 
@@ -13,8 +14,16 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         assert collector.books_genre.get('Гордость и предубеждение и зомби') == ''
 
-    def test_set_book_genre_set_cartoon(self):
+    def test_set_book_genre_set_cartoon(self): # устанавливаем книге жанр
         collector = BooksCollector()
         collector.add_new_book('Жил-был пёс')
         collector.set_book_genre('Жил-был пёс', 'Мультфильмы')
         assert collector.books_genre['Жил-был пёс'] == 'Мультфильмы'
+
+    def test_get_book_genre_check_comedy(self):
+        collector = BooksCollector()
+        collector.add_new_book('Один дома')
+        collector.set_book_genre('Один дома', 'Комедии')
+        assert collector.get_book_genre('Один дома') == 'Комедии'
+
+
