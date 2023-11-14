4a7cd2a0176d5bf2cafc8c466e6a4aca2f5cf4c8
Anastasiya Nikitina <anastasia.nikitina@aliexpress.ru>
2023-11-14 20:35:59 +0300

    edits after review

--- tests.py
+++ tests.py
@@ -5,18 +5,19 @@ class TestBooksCollector:
     def test_add_new_book_add_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Биба')
-        assert 'Биба' in collector.books_genre
+        collector.add_new_book('Боба')
+        assert len(collector.get_books_genre()) == 2
 
     def test_add_new_book_added_book_has_no_genre_true(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
-        assert collector.books_genre.get('Гордость и предубеждение и зомби') == ''
+        assert list(collector.books_genre.values()) == ['']
 
     def test_set_book_genre_set_cartoon(self):
         collector = BooksCollector()
         collector.add_new_book('Жил-был пёс')
         collector.set_book_genre('Жил-был пёс', 'Мультфильмы')
-        assert collector.books_genre['Жил-был пёс'] == 'Мультфильмы'
+        assert list(collector.books_genre.values()) == ['Мультфильмы']
 
     def test_get_book_genre_get_comedy(self, fill_books_genre):
         collector = BooksCollector()
@@ -49,13 +50,14 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('Котенок по имени Гав')
         collector.add_book_in_favorites('Котенок по имени Гав')
-        assert 'Котенок по имени Гав' in collector.favorites
+        assert len(collector.favorites) == 1
 
     def test_delete_book_from_favorites_delete_one(self):
         collector = BooksCollector()
         collector.favorites.append('Дюна')
+        collector.favorites.append('Игра престолов')
         collector.delete_book_from_favorites('Дюна')
-        assert 'Дюна' not in collector.favorites
+        assert len(collector.favorites) == 1
 
     def test_get_list_of_favorites_books_list_of_two_books(self):
         collector = BooksCollector()
